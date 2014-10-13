99700a6d47679db33ea095f27bb2bbb52c202812
asipple1 <asipple1@gmail.com>
2014-10-12 19:22:17 -0500

    added font

--- JQ/Main.js
+++ JQ/Main.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
 
-// Use enter to add items to the list
-    // click on enter or click on plus sign to add items to the list
 
+    var $new =$('#New');
+      var $item =$('#item');
+    var $add=$('#add');
+    // loads at the start
+        $new.show();
+        $item.hide();
+        $add.hide();
+    // Use enter to add items to the list
+    // click on enter or click on plus sign to add items to the list
 $('#item').keyup(function (event){
     if(event.keyCode ==13){     //click enter
         event.preventDefault();
@@ -14,11 +21,11 @@ $('#item').keyup(function (event){
 		var txtbox = document.getElementById('item');
 		var txtval = txtbox.value;
 		event.preventDefault();
-
 		if(!$.trim($('#item').val())) { //checks to see if the input form is blank
 			alert('Please enter text to add to the list'); // sends message 
 		} else {
-			$('<li class="items"></li>').appendTo('#list').html('<div class="box"></div><span>' + txtval + '</span><img class="trash" src="Images/glyphicons_016_bin.png"/>'); //adds itmes to list
+			$('li:last').after('<li><span>'+ txtval +'</span><img class="trash" src="Images/glyphicons_016_bin.png"/></li>'); //adds itmes to list
+      
 		txtbox.value =''; // removes item from input box
 		};
 	});
@@ -30,8 +37,12 @@ $('#item').keyup(function (event){
     //cross off list items
 	$('#list').on('click', 'li', function(){
         
-        $(this).toggleClass('strike');
-        $('.strike').animate({width:['=50']})
+        $(this).toggleClass('strike');//click to toggle stike class
                                            });
+    $('.info').on('click',function(){
+    $new.hide();
+        $item.show();
+        $add.show();
+    });
     
 }); // End
